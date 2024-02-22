7e6864ea6538d5999d0705e5e965f56e11ab8276
subhadip-kundu <subhadipkundu106@gmail.com>
2024-02-22 19:50:46 +0530

    Add project pokedex

--- src/Services/Constants/projects.js
+++ src/Services/Constants/projects.js
@@ -1,32 +1,46 @@
 import ToDo from "../../Assets/Images/projects/ToDo.png";
 import calculator from "../../Assets/Images/projects/calculator.png";
 import passwordGenerator from "../../Assets/Images/projects/passwordGenerator.png";
+import pokedex from "../../Assets/Images/projects/pokedex.png";
 
 const projects = [
-	{
-		name: "ToDo Application",
-		descriptionEN: "Todo List app with MERN stack for efficient task management. Features user authentication, secure access, and CRUD operations for seamless task handling.",
-		tools: ["React", "Bootstrap", "Node","Mongo"],
-		website: "https://advanced-to-do-host.vercel.app/",
-		github: "https://github.com/subhadip-kundu/ToDo-Advanced",
-		img: ToDo,
-	},
-	{
-		name: "Arithmetic Calculator",
-		descriptionEN: "User-friendly calculator with digital display for basic arithmetic: addition, subtraction, multiplication, and division.",
-		tools: ["HTML", "CSS", "JavaScript"],
-		website: "https://arithmatic-number-calculator.netlify.app",
-		github: "https://github.com/subhadip-kundu/Simple-calculator",
-		img: calculator,
-	},
-	{
-		name: "FitApp",
-		descriptionEN: "JavaScript password generator creates secure, random passwords with letters, numbers, and characters for robust online authentication.",
-		tools: ["React", "Tailwind", "JavaScript"],
-		website: "https://random-custom-password-generator.netlify.app/",
-		github: "https://github.com/subhadip-kundu/Random-custom-password-generator",
-		img: passwordGenerator,
-	},
+  {
+    name: "ToDo Application",
+    descriptionEN:
+      "Todo List app with MERN stack for efficient task management. Features user authentication, secure access, and CRUD operations for seamless task handling.",
+    tools: ["React", "Bootstrap", "Node", "Mongo"],
+    website: "https://advanced-to-do-host.vercel.app/",
+    github: "https://github.com/subhadip-kundu/ToDo-Advanced",
+    img: ToDo,
+  },
+  {
+    name: "Pokédex",
+    descriptionEN:
+      "JavaScript password generator creates secure, random passwords with letters, numbers, and characters for robust online authentication.",
+    tools: ["React", "JavaScript", "PokéAPI"],
+    website: "https://pokedex-by-subh.netlify.app/",
+    github: "https://github.com/subhadip-kundu/Pokedex",
+    img: pokedex,
+  },
+  {
+    name: "Arithmetic Calculator",
+    descriptionEN:
+      "User-friendly calculator with digital display for basic arithmetic: addition, subtraction, multiplication, and division.",
+    tools: ["HTML", "CSS", "JavaScript"],
+    website: "https://arithmatic-number-calculator.netlify.app",
+    github: "https://github.com/subhadip-kundu/Simple-calculator",
+    img: calculator,
+  },
+  {
+    name: "Password Generator",
+    descriptionEN:
+      "JavaScript password generator creates secure, random passwords with letters, numbers, and characters for robust online authentication.",
+    tools: ["React", "Tailwind", "JavaScript"],
+    website: "https://random-custom-password-generator.netlify.app/",
+    github:
+      "https://github.com/subhadip-kundu/Random-custom-password-generator",
+    img: passwordGenerator,
+  },
 ];
 
 export default projects;
