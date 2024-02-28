b16c149059fa75184d6ea33d73ea0e8bf2a85017
subhadip-kundu <subhadipkundu106@gmail.com>
2024-02-28 23:53:28 +0530

    Update some information

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,8 +7,8 @@ export default function Contact({ content, refProperty }) {
     <section className='section-container contact-section' ref={refProperty}>
       <h2 className='section-title'>{content.title}</h2>
       <p className='contact-text'>{content.paragraph}</p>
-      <p className='mail'>Subhadipkundu106@gmail.com</p>
-      <Button value={content.button} type='solid' link='mailto:Subhadipkundu106@gmail.com' />
+      <p className='mail'>Subhadipkundu1107@gmail.com</p>
+      <Button value={content.button} type='solid' link='mailto:Subhadipkundu1107@gmail.com' />
       <div className='all-social-links'>
         {social.map((s, index) => (
           <a key={index} href={s.link} target='_blank' rel='noreferrer'>
