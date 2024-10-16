d3dfbc79f449c232a8d71a56bff081e97c2e964a
subhadip-kundu <subhadipkundu106@gmail.com>
2024-10-16 17:24:36 +0530

    little fix

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -5,27 +5,28 @@ import dateFormat from "../Services/Utils/dateFormat";
 
 export default function Experience({ content, refProperty }) {
   return (
-    <section style={{height:"85vh",display:"flex",justifyContent:"center"}} className='section-container experience-section' ref={refProperty}>
-
-       <h2 className='section-title'>{content.title}</h2>
-      <div className='jobs-container'>
+    <section
+      style={{ height: "85vh", display: "flex", justifyContent: "center" }}
+      className="section-container experience-section"
+      ref={refProperty}
+    >
+      <h2 className="section-title">{content.title}</h2>
+      <div className="jobs-container">
         {jobs.map((j, index) => (
           <Job
             key={index}
             companyName={j.companyName}
             companyLink={j.companyLink}
-            date={`${dateFormat(j.date[0], content.language)} - ${dateFormat(j.date[1], content.language)}`}
+            date={`${dateFormat(j.date[0], content.language)} - ${dateFormat(
+              j.date[1],
+              content.language
+            )}`}
             position={j.positionEN}
             tasks={j.tasksEN}
           />
         ))}
-        <div className='time-line'></div>
-      </div> 
-
-    //   {/* END */}
-
+        <div className="time-line"></div>
+      </div>
     </section>
   );
 }
-
-
